c200e4cec9819f54de8a920a3bf39c174b2ee8fe
ravsh <ravshanabdurashidov6@gmail.com>
2023-06-29 17:13:08 +0500

    Calculator_Methods

---
+++ CalculatorApp/CalculatorArithmetic.cs
@@ -0,0 +1,45 @@
+﻿namespace CalculatorApp;
+
+public class CalculatorArithmetic
+{
+    public static double Add(params double[] num)
+   => num.Length == 0 ? 0 : num.Sum();
+    public static double Subtract(params double[] num)
+    {
+        var arxivnum = num[0];
+        if (num.Length == 0) throw new InvalidCastException("Error!!");
+        for (int i = 0; i < num.Length - 1; i++)
+        {
+            if (i == 0 && num[0] == 0) return 0;
+            if (num[i] == 0) throw new DivideByZeroException("Do not input a zero!! ");
+            arxivnum -= num[i + 1];
+        }
+        return arxivnum;
+    }
+    public static double Divide(params double[] num)
+    {
+        var arxivnum = num[0];
+
+        if (num.Length == 0) throw new InvalidCastException("Error!!");
+
+        for (int i = 0; i < num.Length - 1; i++)
+        {
+            if (i == 0 && num[0] == 0) return 0;
+            if (num[i] == 0) throw new DivideByZeroException("Do not input a zero!!");
+            arxivnum /= num[i + 1];
+        }
+        return arxivnum;
+    }
+    public static double Multiplaction(params double[] num)
+    {
+        var arxivnum = num[0];
+        if (num.Length == 0) throw new InvalidCastException("Error!!");
+        for (int i = 0; i < num.Length - 1; i++)
+        {
+            if (i == 0 && num[0] == 0) return 0;
+            if (num[i] == 0) throw new DivideByZeroException("Do not input a zero");
+            arxivnum *= num[i + 1];
+        }
+        return arxivnum;
+    }
+}
---
+++ CalculatorApp/Program.cs
@@ -0,0 +1,48 @@
+﻿
+
+namespace CalculatorApp
+{
+    public class Calculator
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.Write("Misol uchun 5 + 7\n");
+                string expression = Console.ReadLine();
+                Console.WriteLine("Result: " + (double)EvaluateExpression(expression));
+
+                ConsoleKeyInfo key = Console.ReadKey();
+                if (key.KeyChar == 'q' || key.KeyChar == 'Q')
+                    break;
+            }
+        }
+
+        private static double EvaluateExpression(string expression)
+        {
+            string[] parts = expression.Split(' ');
+
+            double son1, son2;
+
+            son1 = double.Parse(parts[0]);
+            son2 = double.Parse(parts[2]);
+
+
+            char MatematikAmal = parts[1][0];
+
+            switch (MatematikAmal)
+            {
+                case '+':
+                    return CalculatorArithmetic.Add(son1, son2);
+                case '-':
+                    return CalculatorArithmetic.Subtract(son1, son2);
+                case '*':
+                    return CalculatorArithmetic.Multiplaction(son1, son2);
+                case '/':
+                    return CalculatorArithmetic.Divide(son1, son2);
+                default:
+                    throw new ArgumentException("Invalid operator: ");
+            }
+        }
+    }
+}
